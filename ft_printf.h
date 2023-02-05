2a8006e0fcacc00abbd5d17cc26abb9e08887d11
Kohshi Yamaguchi <kyamaguc@c1r22s2.42tokyo.jp>
2023-02-05 16:33:59 +0900

    norm

--- ft_printf.h
+++ ft_printf.h
@@ -16,7 +16,6 @@
 # include <unistd.h>
 # include <limits.h>
 
-// todo: check if the return value of the write() is size_t or ssize_t...!!!
 size_t	ft_putchar(char c);
 size_t	ft_putstr(char *s);
 int		ft_printf(const char *str, ...);
