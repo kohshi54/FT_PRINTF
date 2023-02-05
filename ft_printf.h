587ac6c4b7ebb0a52879e2abad902490a83ff771
Kohshi Yamaguchi <kyamaguc@c1r22s2.42tokyo.jp>
2023-02-05 17:09:48 +0900

    norm

--- ft_printf.h
+++ ft_printf.h
@@ -20,5 +20,5 @@ size_t	ft_putchar(const char c);
 size_t	ft_putstr(const char *s);
 int		ft_printf(const char *str, ...);
 size_t	ft_putnbr(int nbr);
-size_t	ft_convert_base(unsigned long long num, const size_t base, const char format);
+size_t	ft_convert_base(unsigned long long num, const size_t base, char format);
 #endif
